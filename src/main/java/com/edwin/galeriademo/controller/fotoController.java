601af0262f4a403c5ad70ce8fc67f94ea0e44570
EdwinNM22 <edwin.nunez@catolica.edu.sv>
2025-03-20 12:09:26 -0600

    vista para agregar fotos y controlador

---
+++ src/main/java/com/edwin/galeriademo/controller/fotoController.java
@@ -0,0 +1,16 @@
+package com.edwin.galeriademo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fotos")
+public class fotoController {
+
+    @GetMapping("")
+    public String show() {
+
+        return "fotos/show";
+    }
+}
