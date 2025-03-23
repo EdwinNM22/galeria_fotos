3bc27803fccb941c6e88f4109361cb44c11a3659
EdwinNM22 <edwin.nunez@catolica.edu.sv>
2025-03-22 22:04:12 -0600

    funcion login de usuario

--- src/main/java/com/edwin/galeriademo/controller/usuarioController.java
+++ src/main/java/com/edwin/galeriademo/controller/usuarioController.java
@@ -2,6 +2,7 @@ package com.edwin.galeriademo.controller;
 
 import com.edwin.galeriademo.model.usuario;
 import com.edwin.galeriademo.service.IUsuarioService;
+import jakarta.servlet.http.HttpSession;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Optional;
 import java.util.logging.Logger;
 
 @Controller
@@ -23,10 +25,34 @@ public class usuarioController {
         return "usuario/registro";
     }
 
-    @PostMapping("usuario/save")
+    @PostMapping("/usuario/save")
     public String save(usuario usuario) {
         usuario.setTipo_usuario("USER");
         usuarioService.save(usuario);
         return "redirect:/";
     }
+
+    @GetMapping("/login")
+    public String login() {
+        return "usuario/login";
+    }
+
+    @PostMapping("/acceder")
+    public String acceder(usuario usuario, HttpSession session) {
+        Optional<usuario> user = usuarioService.findByEmail(usuario.getEmail());
+
+        if (user.isPresent() && user.get().getPassword().equals(usuario.getPassword())) { // Asegúrate de que la contraseña se verifique
+            session.setAttribute("idusuario", user.get().getId());
+
+            if (user.get().getTipo_usuario().equals("ADMIN")) {
+                return "redirect:/adm";
+            } else {
+                return "redirect:/";
+            }
+        } else {
+            // Manejo de error: redirigir a la página de acceso con un mensaje de error
+            return "redirect:/"; // Asegúrate de tener una página de login que maneje este parámetro
+        }
+    }
 }
+
