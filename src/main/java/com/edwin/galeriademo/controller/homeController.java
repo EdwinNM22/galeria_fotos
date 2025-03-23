1893be3ec19deb1494666e89b03b79f7f8c243e1
EdwinNM22 <edwin.nunez@catolica.edu.sv>
2025-03-22 19:03:49 -0600

    Mejoras generales

---
+++ src/main/java/com/edwin/galeriademo/controller/homeController.java
@@ -0,0 +1,26 @@
+package com.edwin.galeriademo.controller;
+
+import com.edwin.galeriademo.model.album;
+import com.edwin.galeriademo.service.albumService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/")
+public class homeController {
+
+    @Autowired
+    private albumService albumService;
+
+    @GetMapping("")
+    public String home(Model model) {
+        List<album> albumes = albumService.findAll(); // Obtener todos los álbumes
+        model.addAttribute("albumes", albumes); // Pasar los álbumes al modelo
+        return "usuario/home"; // Asegúrate de que esta vista esté configurada para mostrar álbumes
+    }
+}
